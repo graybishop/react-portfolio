50eeb22c09e0896d0a3f2cca6a1c2980208cb049
Jake Swackhammer <j.100101@gmail.com>
2021-11-23 16:35:18 -0500

    styled contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,33 +5,44 @@ const Contact = () => {
     name: '',
     email: '',
     message: ''
-  })
+  });
 
   const handleInputChange = (event) => {
     const target = event.target;
     const value = target.value;
     const name = target.name;
 
-    let newValues = formValues
-    newValues[name] = value
+    let newValues = formValues;
+    newValues[name] = value;
 
-    setFormValues({...newValues})
-  }
+    setFormValues({ ...newValues });
+  };
 
-  return(
-    <section>
-      <h2 className="text-3xl">Contact Me</h2>
-      <form action="">
-        <label htmlFor="name">Your Name:</label>
-        <input type="text" name="name" value={formValues.name}  onChange={handleInputChange} required/>
-        <label htmlFor="email">Your Name:</label>
-        <input type="email" name="email" id="" value={formValues.email}  onChange={handleInputChange} required/>
-        <label htmlFor="message">Leave a message:</label>
-        <textarea name="message" id="" value={formValues.message} onChange={handleInputChange}/>
-        <button type="submit">Submit</button>
+  return (
+    <section className="bg-black-coffee-dark text-timberwolf container mx-auto pb-6">
+      <div className='headerDiv'>
+        <h2>Contact Me</h2>
+        <i class="far fa-comment-dots"></i>
+      </div>
+      <form action="" className="flex flex-col gap-2 p-4">
+        <div>
+          <label htmlFor="name">Your Name</label>
+          <input type="text" name="name" value={formValues.name} onChange={handleInputChange} required />
+        </div>
+        <div>
+          <label htmlFor="email">Your Email</label>
+          <input type="email" name="email" id="" value={formValues.email} onChange={handleInputChange} required />
+        </div>
+        <div>
+          <label htmlFor="message">Leave a message</label>
+          <textarea name="message" id="" value={formValues.message} onChange={handleInputChange} />
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
       </form>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
